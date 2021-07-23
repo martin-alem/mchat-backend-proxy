0d599128e7fb7b68536d22809802b01ed24cef4f
martin-alem <alemajohmartin@gmail.com>
2021-07-23 12:47:32 -0400

    used express cors middleware

--- app.js
+++ app.js
@@ -13,23 +13,18 @@ const authRoute = require(path.join(__dirname, "routes/authRoute"));
 const signupRoute = require(path.join(__dirname, "routes/signupRoute"));
 const resetRoute = require(path.join(__dirname, "routes/resetRoute"));
 
+var corsOptions = {
+    origin: 'http://192.168.0.71:3000',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions));
+
 //app settings
 app.set("x-powered-by", false);
 
 //parse json payloads and makes it available on the req object.
 app.use(express.json());
-
-/**
- * Manual cors configuration
- */
-app.options("*", (req, res) => {
-    res.status(200);
-    res.set("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
-    res.set("Access-Control-Allow-Credentials", true);
-    res.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    res.set("Access-Control-Allow-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Methods, Access-Control-Allow-Credentials, Content-Type");
-    res.end();
-});
 
 //Authentication Route middleware
 app.use("/auth", authRoute);
